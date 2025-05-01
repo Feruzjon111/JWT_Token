207883c3ad7ef0e9b243a668239adb8b32fd661b
Feruzjon111 <feruzjonmuzaffarov1209@gmail.com>
2025-05-01 19:02:46 +0500

    JWT

---
+++ auth_app/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth import authenticate
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data['username']
+        password = data['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return Response({"data": user.username, "msg": "Siz ro'yxatdan o'tdingiz!"})
+
+# class RegisterView(APIView):
+#     def post(self, request):
+#         serializer = RegisterSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"data": serializer.data, "msg": "Siz ro'yxatdan o'tdingiz!"})
+#         return Response({"msg": "Xatolik"})
+
+
+class LoginView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data['username']
+        password = data['password']
+
+        user = authenticate(username= username, password= password)
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token)
+        })
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = JWTAuthentication,
+
+    def post(self, request):
+        try:
+            r_token = request.data['refresh']
+            token = RefreshToken(r_token)
+            token.blacklist()
+            return Response({
+                "msg": "Tizimdan chiqdingiz!"
+            })
+        except Exception:
+            return Response({
+                "msg": "Token noto'g'ri!"
+            })
